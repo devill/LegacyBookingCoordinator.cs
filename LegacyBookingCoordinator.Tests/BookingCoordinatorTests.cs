698178b338b18a1ab470301198d3b716d2901ce6
Ivett Ördög <devillmeister@gmail.com>
2025-08-24 21:39:49 +0200

    Gold solution

--- LegacyBookingCoordinator.Tests/BookingCoordinatorTests.cs
+++ LegacyBookingCoordinator.Tests/BookingCoordinatorTests.cs
@@ -17,130 +17,31 @@ namespace LegacyBookingCoordinator.Tests
             var specialRequests = "meal,wheelchair";
             var bookingDate = new DateTime(2025, 03, 04, 14, 00, 56);
 
-            var storybook = new StringBuilder();
-            var cl = new CallLogger(storybook);
+            var callLog = CallLog.FromVerifiedFile();
 
             var factory = ObjectFactory.Instance();
 
             try
             {
-                factory.SetOne(cl.Wrap<IBookingRepository>(new BookingRepositoryStub(), "💾"));
-                factory.SetOne(cl.Wrap<IPartnerNotifier>(new PartnerNotifierStub(), "📣"));
-                factory.SetOne(cl.Wrap<IAuditLogger>(new AuditLoggerStub(), "🪵"));
-                factory.SetOne(cl.Wrap<IFlightAvailabilityService>(new FlightAvailabilityServiceStub(), "✈️"));
+                factory.SetOne(Parrot.Create<IBookingRepository>(callLog, "💾"));
+                factory.SetOne(Parrot.Create<IPartnerNotifier>(callLog, "📣"));
+                factory.SetOne(Parrot.Create<IAuditLogger>(callLog, "🪵"));
+                factory.SetOne(Parrot.Create<IFlightAvailabilityService>(callLog, "✈️"));
                 factory.SetOne<Random>(new RandomStub());
 
                 var coordinator = new BookingCoordinator(bookingDate);
                 var booking = coordinator.BookFlight(passengerName, flightNumber, departureDate,
                     passengerCount, airlineCode, specialRequests);
 
-                storybook.AppendLine(booking.ToString());
+                callLog.AppendLine(booking.ToString());
 
                 // Assert
-                await Verify(storybook.ToString());
+                await callLog.Verify();
             }
             finally
             {
                 factory.ClearAll();
             }
-        }
-    }
-
-    public class AuditLoggerStub : IAuditLogger, IConstructorCalledWith
-    {
-        public void LogBookingActivity(string activity, string bookingReference, string userInfo)
-        {
-        }
-
-        public void RecordPricingCalculation(string calculationDetails, decimal finalPrice, string flightInfo)
-        {
-        }
-
-        public void LogErrorWithAlert(Exception ex, string context, string bookingRef)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void FlushAndArchiveLogs()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ConstructorCalledWith(ConstructorParameterInfo[] parameters)
-        {
-            
-        }
-    }
-
-    public class PartnerNotifierStub : IPartnerNotifier, IConstructorCalledWith
-    {
-        public void NotifyPartnerAboutBooking(string airlineCode, string bookingReference, decimal totalPrice,
-            string passengerName,
-            string flightDetails, bool isRebooking = false)
-        {
-        }
-
-        public bool ValidateAndNotifySpecialRequests(string airlineCode, string specialRequests, string bookingRef)
-        {
-            return true;
-        }
-
-        public void UpdatePartnerBookingStatus(string airlineCode, string bookingRef, string newStatus)
-        {
-        }
-
-        public void ConstructorCalledWith(ConstructorParameterInfo[] parameters)
-        {
-            
-        }
-    }
-
-    public class FlightAvailabilityServiceStub : IFlightAvailabilityService, IConstructorCalledWith
-    {
-        public List<string> CheckAndGetAvailableSeatsForBooking(string flightNumber, DateTime departureDate,
-            int passengerCount)
-        {
-            return ["11A", "11B"];
-        }
-
-        public bool IsFlightFullyBooked(string flightNumber, DateTime departureDate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ConstructorCalledWith(ConstructorParameterInfo[] parameters)
-        {
-            
-        }
-    }
-
-    public class BookingRepositoryStub : IBookingRepository, IConstructorCalledWith
-    {
-        public string SaveBookingDetails(string passengerName, string flightDetails, decimal price,
-            DateTime bookingDate)
-        {
-            return "DRW6N";
-        }
-
-        public Dictionary<string, object> GetBookingInfo(string bookingReference)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool ValidateAndEnrichBookingData(string bookingRef, out decimal actualPrice,
-            out string enrichedFlightInfo)
-        {
-            throw new NotImplementedException();
-        }
-
-        public decimal GetHistoricalPricingData(string flightNumber, DateTime date, int dayRange)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ConstructorCalledWith(ConstructorParameterInfo[] parameters)
-        {
-            
         }
     }
 
